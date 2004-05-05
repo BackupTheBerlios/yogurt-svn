569122a6c2f56af4a400b1ecd27a17f96f383e72
perky <perky@af04c2ef-cfd4-0310-aada-b65faf30c9a0>
2004-05-05 04:28:02 +0000

    Add Package frame directories.

---
+++ Yogurt/Yogurt/Commands/__init__.py
@@ -0,0 +1,8 @@
+#
+# Yogurt/Commands/__init__.py
+#  - Package initializer for Yogurt.Commands.
+#
+# This file is part of Yogurt.
+#
+# $Id$
+#
---
+++ Yogurt/Yogurt/__init__.py
@@ -0,0 +1,8 @@
+#
+# Yogurt/__init__.py
+#  - Package initializer for Yogurt.
+#
+# This file is part of Yogurt.
+#
+# $Id$
+#
